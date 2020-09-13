08e70e3a062af9ba36b42af603c66e9604294d4b
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-09-13 21:08:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,17 @@
 def line(queue)
   if queue[0] === nil
     return "The line is currently empty."
-  elsif
   else
+    return "The line is currently: #{queue}"
   end
 end
 
 def take_a_number(queue, name)
   position = queue.length + 1
   queue << "#{position}. #{name}"
-  return "Welcome, #{name}. You are number #{position}"
+  return "Welcome, #{name}. You are number #{position} in line."
+end
+
+def now_serving
+  puts "Currently serving #{array.shift.split(". ")[1]}"
 end
