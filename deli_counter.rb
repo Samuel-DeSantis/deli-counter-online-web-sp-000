901b031bea8a821773b301f6609b8010e6d829a9
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-09-13 21:01:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(queue)
+  if queue[0] === nil
+    return "The line is currently empty."
+  elsif 
+  else
+  end
+end
+
+def take_a_number(queue, name)
+  position = queue.length + 1
+  queue << "#{position}. #{name}"
+  return "Welcome, #{name}. You are number #{position}"
+end
